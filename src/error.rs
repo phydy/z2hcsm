dfc798213f088d0140e18956cee589fa7d61a0f6
Phidel Musungu <mphidel@gmail.com>
2022-08-03 22:10:01 +0300

    zero to hero exercise

---
+++ src/error.rs
@@ -0,0 +1,20 @@
+use cosmwasm_std::StdError;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ContractError {
+    #[error("{0}")]
+    Std(#[from] StdError),
+
+    #[error("Custom Error val: {val:?}")]
+    CustomError { val: String },
+
+    #[error("Too many poll options")]
+    TooManyOptions {},
+
+    #[error("Poll not found")]
+    PollNotFound {},
+
+    #[error("Unauthorized")]
+    Unauthorized {},
+}
